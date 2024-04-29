1ec3b89e067a133086604df5cd91e3968e8ce76d
Nicolie <143574745+naoquerooxe@users.noreply.github.com>
2024-04-29 16:25:54 -0300

    Add files via upload

---
+++ Modelagem de sistemas/apta a votar/questao3.py
@@ -0,0 +1,6 @@
+anoNascimento = int(input("Informe o seu ano de nascimento:"))
+
+if anoNascimento > 2008:
+    print("Você não esta apto(a) a votar.")
+else:
+    print("Você esta apto(a) a votar.")
---
+++ Modelagem de sistemas/primeiro divisível pelo segundo/questao5.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite primeiro número:"))
+numero2 = int(input("Digite segundo número:"))
+
+
+if numero1 % numero2 == 0:
+    print("O primeiro número é divisível pelo segundo número")
+else:
+    print("O primeiro número não é divisível pelo segundo número")
